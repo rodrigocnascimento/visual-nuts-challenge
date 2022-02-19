7ab7958abe650cb71f86d2c21266212d425b340b
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2022-02-19 10:27:25 -0300

    feat(language): get the country with most language and speak German

--- countries-challenge.js
+++ countries-challenge.js
@@ -10,4 +10,33 @@ CountryHelper.getNumberOfCountries = function () {
   return countries.length;
 };
 
+CountryHelper.findCountriesByLanguage = function (language) {
+  const countriesList = CountryHelper.getCountries();
+
+  return countriesList
+    .map((country) => country.languages.includes(language) && country)
+    .filter(Boolean);
+};
+
+CountryHelper.getCountryWithMoreOfficialLanguages = function (countriesList) {
+  let maxLanguages = 0;
+  let country = {};
+
+  for (let i = 0; i < countriesList.length; i++) {
+    let officialLanCounter = countriesList[i].languages.length;
+
+    if (officialLanCounter > maxLanguages) {
+      maxLanguages = officialLanCounter;
+      country = countriesList[i];
+    }
+  }
+  return country;
+};
+
+CountryHelper.getCountryWithMostOfficialLanguagesThatTalksGerman = function () {
+  const germanCountries = CountryHelper.findCountriesByLanguage("de");
+
+  return CountryHelper.getCountryWithMoreOfficialLanguages(germanCountries);
+};
+
 module.exports = CountryHelper;
--- countries-challenge.spec.js
+++ countries-challenge.spec.js
@@ -1,15 +1,56 @@
 // Write a function in javascript that:
 // - returns the number of countries in the world
 // - finds the country with the most official languages, where they officially speak German (de)
-// - that counts all the official languages spoken in the listed countries.
+// that counts all the official languages spoken in the listed countries.
 // - to find the country with the highest number of official languages.
 // - to find the most common official language(s), of all countries
 
 const countryHelper = require("./countries-challenge");
+const countriesMock = require("./countries.json");
+
 test("should return the number of countries in the world", () => {
   expect(countryHelper.getNumberOfCountries()).toEqual(5);
 });
 
-test("returns the number of countries in the world", () => {
-  expect(countriesChallenge.getNumberOfCountries()).toEqual(5);
+test("should find the country with the most official languages, where they officially speak German (de) that counts all the official languages spoken in the listed countries.", () => {
+  const countryWithMostOfficialLanguagesThatTalksGerman = {
+    country: "BE",
+    languages: ["nl", "fr", "de"],
+  };
+
+  expect(
+    countryHelper.getCountryWithMostOfficialLanguagesThatTalksGerman()
+  ).toEqual(countryWithMostOfficialLanguagesThatTalksGerman);
+});
+
+test("should return countries list that speak NL", () => {
+  const nlLangCountries = [
+    {
+      country: "BE",
+      languages: ["nl", "fr", "de"],
+    },
+    { country: "NL", languages: ["nl"] },
+  ];
+
+  jest.spyOn(countryHelper, "getCountries").mockReturnValue([...countriesMock]);
+
+  expect(countryHelper.findCountriesByLanguage("nl")).toEqual(nlLangCountries);
+});
+
+test("should return countries list that speak DE", () => {
+  const deLangCountries = [
+    {
+      country: "BE",
+      languages: ["nl", "fr", "de"],
+    },
+    { country: "DE", languages: ["de"] },
+  ];
+
+  const countriesByLanguage = [...countriesMock];
+
+  jest
+    .spyOn(countryHelper, "getCountries")
+    .mockReturnValue(countriesByLanguage);
+
+  expect(countryHelper.findCountriesByLanguage("de")).toEqual(deLangCountries);
 });
